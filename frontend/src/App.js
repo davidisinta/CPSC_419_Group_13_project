fa5bf674226837a7c5c5684e4eae7e9de7abb9c2
David Isinta Nyakawa <83705759+davidisinta@users.noreply.github.com>
2024-05-03 20:38:35 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,13 +7,11 @@ import Update from './components/pages/update';
 import Footer from './components/footer';
 import Login from "./components/pages/Login";
 import About from "./components/pages/about";
-<<<<<<< HEAD
 import Map from "./components/pages/map"
 import Profile from "./components/pages/profile";
-=======
 import Map from "./components/pages/map";
 import Shift from "./components/pages/shift";
->>>>>>> main
+
 
 
 function App() {
@@ -35,22 +33,14 @@ function App() {
       <Router>
         <NavBar handleZoneChange={handleZoneChange} setLoginStatus={handleLoginStatus} loginStatus={isLoggedIn} />
         <Routes>
-<<<<<<< HEAD
+
           <Route path="/" element={<Home currentZone={displayZone}/>} />
           <Route path="/login" element={<Login/>} />
           <Route path="/profile" element={<Profile/>} />
           <Route path="/update/:id" element={<Update/>} />
           <Route path="/about" element={<About/>} />
           <Route path="/map" element={<Map/>}/>
-=======
-          <Route path="/" element={<Home currentZone={displayZone} />} />
-          <Route path="/report" element={<Report />} />
-          <Route path="/update/:id" element={<Update />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/map" element={<Map />} />
-          <Route path="/shift" element={<Shift />} />
-          <Route path="/profile" element={<Profile setLoginStatus={handleLoginStatus} loginStatus={isLoggedIn} />} />
->>>>>>> main
+
         </Routes>
         <Footer />
       </Router>
